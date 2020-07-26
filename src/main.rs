3ffdf0bd37b2ba37c9e5b8cd95c54c9d6c4c7fdf
sakuraya <bluepixel1214@gmail.com>
2020-07-26 15:19:35 +0900

    20.1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let (headers, filename) = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut body = String::new();
+    file.read_to_string(&mut body).unwrap();
+    
+    let response = format!("{}\r\n{}", headers, body);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
