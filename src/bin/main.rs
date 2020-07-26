1f2286ec2f116244282de6ebe73045f1da26f03a
sakuraya <bluepixel1214@gmail.com>
2020-07-26 16:36:05 +0900

    20.2.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,19 @@
+extern crate ezio;
+use ezio::ThreadPool;
 use std::fs::File;
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -14,7 +21,13 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let (headers, filename) = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+    let root = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (headers, filename) = if buffer.starts_with(root) {
+        ("HTTP/1.1 200 OK\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(10));
         ("HTTP/1.1 200 OK\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
